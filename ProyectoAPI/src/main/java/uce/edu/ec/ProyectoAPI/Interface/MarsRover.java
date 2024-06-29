13545b9724a6cf70ac6491f3ff72870881d52735
gabyrodriguez7 <glrodriguezc@uce.edu.ec>
2024-06-28 22:29:33 -0500

    Final

--- ProyectoAPI/src/main/java/uce/edu/ec/ProyectoAPI/Interface/MarsRover.java
+++
@@ -1,9 +0,0 @@
-package uce.edu.ec.ProyectoAPI.Interface;
-
-import java.util.List;
-import java.util.Map;
-
-public interface MarsRover {
-    String getRoverName();
-    Map<String, List<uce.edu.ec.ProyectoAPI.Model.MarsRover>> fetchPhotosByCameraAndSol(String camera, int sol);
-}
